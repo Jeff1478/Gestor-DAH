7c2a0dc67a38e74acefd0211baa3f1cfd61d90b0
Jeff1478 <82961884+Jeff1478@users.noreply.github.com>
2021-08-12 19:32:46 -0600

    Primer Deploy
    Prueba

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+import packageInfo from '../../auth_config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain: packageInfo.domain,
+    clientId : packageInfo.clientId,
+    redirectUri: window.location.origin,
+  },
 };
