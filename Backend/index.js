bc74c6d7042a8f92c2b41f814190fe26ef21ba0a
Jeff1478 <82961884+Jeff1478@users.noreply.github.com>
2021-08-12 20:57:09 -0600

    Neflify
    Publicación del Sitio

--- Backend/index.js
+++ Backend/index.js
@@ -6,8 +6,8 @@ var port = 3900;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/colecciones_dah', { useNewUrlParser: true })
-mongoose.connect('mongodb+srv://museo:museo@cluster0.eba15.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/colecciones_dah', { useNewUrlParser: true })
+// mongoose.connect('mongodb+srv://museo:museo@cluster0.eba15.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true })
 .then(() => {
     console.log('Conexion Exitosa');
 
