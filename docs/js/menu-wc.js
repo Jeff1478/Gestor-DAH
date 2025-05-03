69b08883f927119c278adbc9fe064a0f377c248f
Jeffrey Tapia <jtapia@museocostarica.go.cr>
2025-05-02 18:57:57 -0600

    Actualizo documentación Compodoc

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">angular documentation</a>
+                    <a href="index.html" data-type="index-link">gestordah documentation</a>
                 </li>
 
                 <li class="divider"></li>
